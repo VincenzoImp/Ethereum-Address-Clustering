7be38237226a1a3ff0e62b1bae2e1bd385879596
VincenzoImp <imperati.1834930@studenti.uniroma1.it>
2022-11-24 20:06:03 +0100

    update

--- script.py
+++ script.py
@@ -4,7 +4,6 @@ from web3.middleware import geth_poa_middleware
 from multiprocessing import Pool, Manager
 from tqdm import tqdm
 
-
 chain = 'eth'
 query_mode = 'http'
 core_number = 25
@@ -14,18 +13,17 @@ data = {
         'rpc': '/eth/eth_node/node/geth.ipc',
         'http': 'https://mainnet.infura.io/v3/e0a4e987f3ff4f4fa9aa21bb08f09ef5',
         'datafile': 'data/one_day_exit_scam_eth.csv',
-        'edgefile': 'data/tx_eth.csv',
-        'nodefile': 'data/address_eth.csv'
+        'edgefile': 'data/transactions_eth.csv',
+        'nodefile': 'data/addresses_eth.csv'
     },
     'bsc': {
         'rpc': '',
         'http': 'https://bsc-dataseed.binance.org/',
-        'datafile': 'one_day_exit_scam_bsc.csv',
-        'edgefile': 'data/tx_bsc.csv',
-        'nodefile': 'data/address_bsc.csv'
+        'datafile': 'data/one_day_exit_scam_bsc.csv',
+        'edgefile': 'data/transactions_bsc.csv',
+        'nodefile': 'data/addresses_bsc.csv'
     }
 }
-
 
 def get_w3():
     tmp = data[chain][query_mode]
@@ -41,10 +39,9 @@ def get_w3():
     }
     return w3_data[chain][query_mode]
 
-
 def preprocessing():
     datafile = data[chain]['datafile']
-    df = pd.read_csv(datafile)
+    df = pd.read_csv(datafile, dtype={'address':str, 'block_number_remove':int})
     address_df = df.\
         sort_values(["block_number_remove"]).\
         drop_duplicates(subset=["from"], keep="last").\
@@ -54,29 +51,28 @@ def preprocessing():
     address_df["level"] = 0
     return address_df
 
-
 def task(parameters):
     start, lock, step, max_block_heigth, new_level, curr_level_address_set, curr_level_address_df = parameters
     new_level_address_subset = set()
     new_level_address_subdf = pd.DataFrame.from_dict({"address": [], "use_untill": [], "level": []})
-    for block_number in range(min(start+step-1, max_block_heigth), start-1, -1):
+    for block_number in range(start, min(start+step, max_block_heigth+1)):
         block = w3.eth.get_block(block_number)
-        for transaction in block.transactions[::-1]:
+        local_filtered_curr_level_addresses = curr_level_address_df[curr_level_address_df["use_untill"]>=block_number]["address"].values
+        for transaction in block.transactions:
             tx = w3.eth.get_transaction(transaction.hex())
-            if tx["to"] in curr_level_address_set and tx["to"] in curr_level_address_df[curr_level_address_df["use_untill"]<=block_number]["address"].values:
+            if tx["to"] in curr_level_address_set and tx["to"] in local_filtered_curr_level_addresses:
                 address_to_add = tx["from"]
                 tx = {**tx, **w3.eth.get_transaction_receipt(transaction.hex())}
                 with lock:
                     with open(data[chain]['edgefile'], "a", encoding="UTF8") as tx_file:
                         tx_file.write("{},{},{},{},{},{},{},{},{},{}\n".format(
                             tx["from"], tx["to"], w3.fromWei(tx["value"], 'ether'), tx["effectiveGasPrice"], tx["gasUsed"], tx["hash"].hex(), tx["input"][:10], tx["blockNumber"], new_level, tx["status"]
-                            ))
+                        ))
                 if address_to_add not in new_level_address_subset:
                     new_level_address_subset.add(address_to_add)
                     row = pd.DataFrame.from_dict({"address": [address_to_add], "use_untill": [block_number], "level": [new_level]})
                     new_level_address_subdf = pd.concat([new_level_address_subdf, row], ignore_index=True)
     return new_level_address_subdf
-
 
 def multi(depth=2, store_mode ='w'):
     """
@@ -97,12 +93,12 @@ def multi(depth=2, store_mode ='w'):
             tx_file.write("from,to,value,effectiveGasPrice,gasUsed,hash,input,blockNumber,level,status\n")
     if store_mode == "a":
         address_df = pd.read_csv(data[chain]['nodefile'])
-
+    
     curr_level = int(address_df["level"].max())
     curr_level_address_set = set(address_df["address"].values)
-    curr_level_address_df = address_df
+    curr_level_address_df = address_df.copy()
     while curr_level < depth:
-        max_block_heigth = int(address_df["use_untill"].max())
+        max_block_heigth = int(curr_level_address_df["use_untill"].max())
         step = 10000
         new_level = curr_level + 1
         #generate new level of edges and nodes
@@ -117,7 +113,7 @@ def multi(depth=2, store_mode ='w'):
         new_level_address_df = new_level_address_df.\
             sort_values(["use_untill"]).\
             drop_duplicates(subset=["address"], keep="last").\
-            reset_index(drop=True)\
+            reset_index(drop=True)
         #elimina da new_level_address_df le righe che hanno address presenti in address_df
         new_level_address_df = new_level_address_df[~new_level_address_df["address"].isin(address_df["address"])]
         address_df = pd.concat([address_df, new_level_address_df])
@@ -127,5 +123,4 @@ def multi(depth=2, store_mode ='w'):
         curr_level_address_df = new_level_address_df
     return
 
-
-multi(depth=2, store_mode='a')
+multi(depth=1, store_mode='w')
